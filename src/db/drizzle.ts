6b722c6aa86fd1afab5191548ee2c6feabc3e010
Aziz798 <bouazizimohamedaziz76@gmail.com>
2024-04-07 23:23:30 +0100

    final touches

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,7 +1,8 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-
-const sql = neon(process.env.NEON_DATABASE_URL!);
+import {neon} from '@neondatabase/serverless';
+import {drizzle} from 'drizzle-orm/neon-http';
+import 'dotenv/config.js'
+const NEON_DATABASE_URL="postgresql://stories_owner:bce7tDWyLX3G@ep-floral-snowflake-a2rnirhh-pooler.eu-central-1.aws.neon.tech/todo-list?sslmode=require"
+const sql = neon(NEON_DATABASE_URL!);
 
 const db = drizzle(sql);
 
